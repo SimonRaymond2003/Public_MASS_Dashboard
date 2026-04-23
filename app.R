6900e438e0ba061a8c1b7a8b9d3606b8f247b522
Simon Raymond <simon.p.raymond@gmail.com>
2026-04-23 02:14:04 -0400

    to do list

--- app.R
+++ app.R
@@ -11,17 +11,29 @@ shinyApp(ui, server)
 
 # to do
 
-# forcast  model/vars (exp in the rev and  reverse)... do we wannta try rf? etc
-# missing tile on toronto downtown.. data not the app fix
-# methodology/faq
-# splits proof
-# proof of when can apply 
-# define arts/unselect appraoch in top
-# fsa/dis area/csub div/census div
-# citation list
-# shortcut on multipers instead of raw sut data proof (IMPORANT) none for direct others show its the best that can be done without the use matrix
-# the above the assumption is That each sub-domain's supply chain is a weighted mix of its constituent industries' supply chains, in proportion to their output shares.
-# IMPORTANT explain primary vs mix vs custom and kinda say "well this is what most people do etc but we give u the option etc
-# explain the jobs thing indirect and induced are ezpz here but why direct if already know how many people i employ? (potential etc)
+# info (i) on assumptions when we apply multipliers
+# Switch geographic tiles used 
+# state to use primary/mixed and that custom is if really into it... same for exp/rev
+# add names for stats canada multipliers in selecting custom
+# explain jobs direct vs ind/induced etc 
+# does CSA classify jobs differently
+# need sut data for each year.. downloadable?
+# sut update etc 
+# need to fix the mixed weight slider
+# need to be able to clear the multipliers cleaner
 
+#### seperate tab ####
+
+# input multiple at a time and one at a time
+# 
+
+
+# General Notes
+
+# FAQ/methodology - will hand this the PA
+# if spend money out fo the country then you should not be applying them... state this in the methodolgy
+# categorize who uses csa
+# assumptions about applying th emultipliers
+
+# Remove expenditures predictions?
 
